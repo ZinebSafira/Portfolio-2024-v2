7d0e278ca86e726b617a89aa1eb3054e81153f74
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-04-11 14:38:04 +0200

    UPDATE

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,21 @@ import { Link } from 'react-router-dom'
 
 
 const Header = () => {
-  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>Header</header>;
+  return (
+    <header className='bg-pink-200 fixed w-full px-[30px]
+    lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex 
+    items-center'>
+      <div className='flex flex-col lg:flex-row
+      lg:items-center w-full justify-between'>
+        {/* {logo} */}
+        <Link to={'/'}>
+          <img src={Logo} alt="" />
+        </Link>
+      </div>
+    </header>
+
+
+  );
 };
 
 export default Header;
