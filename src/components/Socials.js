cf3ad708e8becad91fd712ef869a6a5cf733298f
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-04-11 19:09:42 +0200

    adding socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { ImLinkedin, ImPhone, ImMail } from 'react-icons/im';
 
 const Socials = () => {
-  return <div className='hidden x1:flex ml-24'>Socials</div>;
+  return (
+    <div className='hidden xl:flex ml-24'>
+      <ul className='flex gap-x-4'>
+        <li>
+          <a href='https://www.linkedin.com/zenouba' target='_blank' rel='noopener noreferrer'>
+            <ImLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href='tel:+123456789' target='_blank' rel='noopener noreferrer'>
+            <ImPhone />
+          </a>
+        </li>
+        <li>
+          <a href='mailto:example@example.com' target='_blank' rel='noopener noreferrer'>
+            <ImMail />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
