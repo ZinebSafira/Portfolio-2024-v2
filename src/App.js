e9d3e388488d0cfbd8dd11379f727f5b69b60c43
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-04-11 14:17:21 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+//import components
+import Header from './components/Header'
+import AnimRoutes from './components/AnimRoutes'
+import MobileNav from './components/MobileNav'
+import Socials from './components/Socials'
+
+//import router
+import {BrowserRouter as Router} from 'react-router-dom';
+
+//import motion
+import {motion} from 'framer-motion';
+
+
+
+
+const App = () => {
+  return <>
+   <Router>
+    <Header/>
+    <AnimRoutes/>
+   </Router>
+  
+  </>
+};
+
+export default App;
