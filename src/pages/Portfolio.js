7383947e6aa1bc233962ec8b0e871cbf66d5daf8
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-04-11 22:17:31 +0200

    about and portfolio update

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,84 @@
-import React from 'react';
+import React from "react";
+
+//import images
+import Image1 from "../img/portfolio/1.png";
+import Image2 from "../img/portfolio/2.png";
+import Image3 from "../img/portfolio/3.png";
+import Image4 from "../img/portfolio/4.png";
+
+//import Link
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
-  return <section className='section bg-green-200'>Portfolio</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto hf relative ">
+        <div
+          className="flex flex-col lg:flex-row h-full
+        items-center justify-start gap-x-24 text-center
+        lg:text-left pt-24 lg:pt-36 pb-8">
+          {/*text*/}
+          <div className="flex flex-col lg:items-start">
+            <h1 className="h1">Portfolio</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem Ipsum is simply dummy text of the printing and{" "}
+              <b>typesetting industry. </b>Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s,
+              <br />
+              <br />
+              when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing Lorem Ipsum
+              passages, and more recently with desktop publishing software like
+              Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </div>
+          {/*Image grid*/}
+          <div className="grid grid-cols-2 lg:gap-2">
+            {/*image */}
+            <div
+              className="max-w-[250px] lg:max-w-[320px]
+              h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image1}
+                alt=""
+              />
+            </div>
+            <div
+              className="max-w-[250px] lg:max-w-[320px]
+              h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image2}
+                alt=""
+              />
+            </div>
+            <div
+              className="max-w-[250px] lg:max-w-[320px]
+              h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image3}
+                alt=""
+              />
+            </div>
+            <div
+              className="max-w-[250px] lg:max-w-[320px]
+              h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image4}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
