1fbebad2ac30d9a271ce2fd6763a32b8a5eb9d0c
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-06-21 18:53:04 +0200

    Update MobileNav.js
    adding animation to the navbar

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { IoMdClose } from 'react-icons/io';
+import { CgMenuRight } from 'react-icons/cg';
+import gsap from 'gsap';
 
-//import icons
-import { IoMdClose } from 'react-icons/io'
-import { CgMenuRight } from 'react-icons/cg'
-
-//immport Link
-import { Link } from 'react-router-dom'
-//import motion
-import { motion } from 'framer-motion'
-
-
-//menu variants 
-
+//menu variants
 const menuVariants = {
   hidden: {
-    x: '100%'
+    x: '100%',
   },
   show: {
     x: 0,
@@ -24,50 +18,74 @@ const menuVariants = {
   },
 };
 
-
-
-
-
 const MobileNav = () => {
   const [openMenu, setOpenMenu] = useState(false);
-  return <nav className='text-primary xl:hidden'>
-    {/* nav open button */}
-    <div onClick={()=> setOpenMenu(true)}
-    className='text-3xl cursor-pointer'>
-      <CgMenuRight />
-    </div>
-    {/* menu */}
-    <motion.div variants={menuVariants}  
-    initial='hidden'
-    animate={openMenu ? 'show' : ''}
-    className='bg-white shadow-2xl w-full absolute
-    top-0 right-0 max-w-xs h-screen z-20'>
-      {/* icon */}
-      <div onClick={()=> setOpenMenu(false)}
-      className='text-4xl absolute z-30 left-4 top-14
-       text-primary cursor-pointer'>
-        <IoMdClose />
-      </div>
-      {/* menu list */}
-      <ul className='h-full flex flex-col justify-center
-      items-center gap-y-8 text-primary font-primary
-      font-bold text-3xl'>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/About'>About</Link>
-        </li>
-        <li>
-          <Link to='/Portfolio'>Portfolio</Link>
-        </li>
-        <li>
-          <Link to='/Contact'>Contact</Link>
-        </li>
-      </ul>
+  const menuRef = useRef(null);
+  const menuItemsRef = useRef([]);
 
-    </motion.div>
-  </nav>
+  useEffect(() => {
+    if (openMenu) {
+      gsap.fromTo(
+        menuRef.current,
+        { opacity: 0 },
+        { opacity: 1, duration: 0.5, delay: 0.2 }
+      );
+      gsap.fromTo(
+        menuItemsRef.current,
+        { x: 50, opacity: 0 },
+        { x: 0, opacity: 1, stagger: 0.2, duration: 0.8, delay: 0.4 }
+      );
+    }
+  }, [openMenu]);
+
+  const addToMenuItemsRef = (el) => {
+    if (el && !menuItemsRef.current.includes(el)) {
+      menuItemsRef.current.push(el);
+    }
+  };
+
+  return (
+    <nav className="text-primary xl:hidden">
+      {/* nav open button */}
+      <div onClick={() => setOpenMenu(true)} className="text-3xl cursor-pointer">
+        <CgMenuRight />
+      </div>
+      {/* menu */}
+      <motion.div
+        variants={menuVariants}
+        initial="hidden"
+        animate={openMenu ? 'show' : ''}
+        className="bg-white shadow-2xl w-full absolute top-0 right-0 h-screen z-20"
+        ref={menuRef}
+      >
+        {/* icon */}
+        <div
+          onClick={() => setOpenMenu(false)}
+          className="text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer"
+        >
+          <IoMdClose />
+        </div>
+        {/* menu list */}
+        <ul
+          className="h-full flex flex-col justify-center items-center gap-y-8 text-primary font-primary font-bold text-3xl"
+          onClick={() => setOpenMenu(false)}
+        >
+          <li ref={addToMenuItemsRef}>
+            <Link to="/">Home</Link>
+          </li>
+          <li ref={addToMenuItemsRef}>
+            <Link to="/About">About</Link>
+          </li>
+          <li ref={addToMenuItemsRef}>
+            <Link to="/Portfolio">Portfolio</Link>
+          </li>
+          <li ref={addToMenuItemsRef}>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </motion.div>
+    </nav>
+  );
 };
 
 export default MobileNav;
