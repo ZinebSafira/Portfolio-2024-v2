bd0de6df390a5e4cd85916ad3e71d5fa5334efd6
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-06-22 22:44:31 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -31,7 +31,7 @@ const Contact = () => {
       <div className="container mx-auto h-full px-4">
         <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left'>
           <motion.div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top top-72 -z-10'></motion.div>
-          <div className='lg:flex-1 lg:pt-32 px-4'>
+          <div className='lg:flex-1 lg:pt-32 px-4 relative'>
             <h1 className='h1'>Contact me</h1>
             <p className='mb-12'>I would love to grab a coffee with you!</p>
             <form className='contact-form flex flex-col gap-y-4' onSubmit={sendEmail}>
@@ -42,7 +42,7 @@ const Contact = () => {
               <textarea className='outline-none border-b border-b-primary bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]' rows='5' placeholder='Your message' name='message' required></textarea>
               <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>Send it</button>
             </form>
-            <div className='success-message opacity-0 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <div className='success-message opacity-0 absolute top-1/2 transform -translate-y-1/2 w-full text-center'>
               <p>Your message is sent and I hope I can answer you shortly!</p>
             </div>
           </div>
